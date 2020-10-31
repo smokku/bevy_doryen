1aa5de56a365cfdf5ad40abf920e959eab99cfce
Tomasz Sterna <tomek@sterna.link>
2020-10-31 15:44:10 +0100

    Load Font using assets system and convert to texture

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,62 @@ pub struct Doryen {
     font_texture: uni_gl::WebGLTexture,
     font_asset: Handle<Font>,
     font_is_loading: bool,
+    pub font_width: u32,
+    pub font_height: u32,
+    pub char_width: u32,
+    pub char_height: u32,
     program: Program,
+}
+
+impl Doryen {
+    pub fn load_font(&mut self, asset_server: Ref<AssetServer>, font_file: &str) {
+        self.font_asset = asset_server.load(font_file);
+        self.font_is_loading = true;
+        println!("Loading Font {:?}", self.font_asset);
+    }
+
+    fn font_loaded(&mut self, font: &mut Font) {
+        println!("Font: {}x{}", font.char_width, font.char_height);
+        self.font_is_loading = false;
+
+        let img = font.img.take().unwrap();
+        if font.char_width != 0 {
+            self.char_width = font.char_width;
+            self.char_height = font.char_height;
+        } else {
+            self.char_width = img.width() as u32 / 16;
+            self.char_height = img.height() as u32 / 16;
+        }
+        self.font_width = img.width() as u32;
+        self.font_height = img.height() as u32;
+        println!(
+            "font size: {:?} char size: {:?}\n",
+            (self.font_width, self.font_height),
+            (self.char_width, self.char_height)
+        );
+        self.gl.active_texture(0);
+        self.gl.bind_texture(&self.font_texture);
+        self.gl.tex_image2d(
+            uni_gl::TextureBindPoint::Texture2d, // target
+            0,                                   // level
+            img.width() as u16,                  // width
+            img.height() as u16,                 // height
+            uni_gl::PixelFormat::Rgba,           // format
+            uni_gl::PixelType::UnsignedByte,     // type
+            &*img,                               // data
+        );
+
+        self.program.bind(
+            &self.gl,
+            &self.consoles[0].con,
+            self.font_width,
+            self.font_height,
+            self.char_width,
+            self.char_height,
+        );
+        self.program
+            .set_texture(&self.gl, uni_gl::WebGLTexture(self.font_texture.0));
+    }
 }
 
 struct MyRoguelike {
@@ -74,9 +129,9 @@ pub fn main() {
         .init_asset_loader::<FontLoader>()
         .add_startup_system_to_stage(startup_stage::PRE_STARTUP, init.thread_local_system())
         .add_resource(MyRoguelike::new())
-        .add_system(input.system())
-        .add_system_to_stage(stage::POST_UPDATE, render.system())
-        .add_system_to_stage(stage::POST_UPDATE, draw.system())
+        .add_system_to_stage(stage::PRE_UPDATE, input.system()) // game inputs
+        .add_system_to_stage(stage::POST_UPDATE, render.system()) // render game state to console
+        .add_system_to_stage(stage::POST_UPDATE, draw.system()) // engine - consoles to screen
         .run();
 }
 
@@ -134,19 +189,22 @@ fn init(_world: &mut World, resources: &mut Resources) {
     con.register_color("red", (255, 92, 92, 255));
     con.register_color("blue", (192, 192, 255, 255));
 
-    let asset_server = resources.get::<AssetServer>().unwrap();
-    let font_asset = asset_server.load("terminal_8x8.png");
-    std::mem::drop(asset_server);
-    println!("Loading Font {:?}", font_asset);
-
-    resources.insert(Doryen {
+    let mut doryen = Doryen {
         consoles: vec![con],
         gl,
         font_texture,
-        font_asset,
-        font_is_loading: true,
+        font_asset: Handle::default(),
+        font_is_loading: false,
+        font_width: 0,
+        font_height: 0,
+        char_width: 0,
+        char_height: 0,
         program,
-    });
+    };
+
+    doryen.load_font(resources.get::<AssetServer>().unwrap(), "terminal_8x8.png");
+
+    resources.insert(doryen);
 }
 
 fn render(game: Res<MyRoguelike>, mut doryen: ResMut<Doryen>) {
@@ -206,25 +264,30 @@ fn render(game: Res<MyRoguelike>, mut doryen: ResMut<Doryen>) {
     // );
 }
 
-fn draw(windows: Res<Windows>, winit_windows: Res<WinitWindows>) {
-    if let Some(window) = windows.get_primary() {
-        let winit_window = winit_windows.get_window(window.id()).unwrap();
-        println!("{:?}", *winit_window);
-        std::process::exit(0);
-
-        // app.add_system_to_stage(
-        //     bevy_render::stage::RENDER,
-        //     render_system.thread_local_system(),
-        // )
-        // https://github.com/jice-nospam/doryen-rs/blob/master/src/app.rs#L438
-        //
-        // https://github.com/bevyengine/bevy/blob/0dba0fe45f60cf06e802e5ff08710290ad7870d6/crates/bevy_wgpu/src/lib.rs#L24
-        // update: https://github.com/bevyengine/bevy/blob/0dba0fe45f60cf06e802e5ff08710290ad7870d6/crates/bevy_wgpu/src/wgpu_renderer.rs#L112
-        // https://github.com/bevyengine/bevy/blob/master/crates/bevy_wgpu/src/wgpu_renderer.rs#L66
-        // winit_window + https://github.com/rust-windowing/glutin/blob/master/glutin_examples/examples/raw_context.rs#L55
-        // https://github.com/grovesNL/glow/blob/main/examples/hello/src/main.rs#L73
-        // + pub window_surfaces: Arc<RwLock<HashMap<WindowId, wgpu::Surface>>>,
+fn draw(mut doryen: ResMut<Doryen>, mut fonts: ResMut<Assets<Font>>) {
+    if doryen.font_is_loading {
+        if let Some(font) = fonts.get_mut(&doryen.font_asset) {
+            doryen.font_loaded(font);
+        }
     }
+    // if let Some(window) = windows.get_primary() {
+    //     // let winit_window = winit_windows.get_window(window.id()).unwrap();
+    //     // println!("{:?}", *winit_window);
+    //     std::process::exit(0);
+
+    //     // app.add_system_to_stage(
+    //     //     bevy_render::stage::RENDER,
+    //     //     render_system.thread_local_system(),
+    //     // )
+    //     // https://github.com/jice-nospam/doryen-rs/blob/master/src/app.rs#L438
+    //     //
+    //     // https://github.com/bevyengine/bevy/blob/0dba0fe45f60cf06e802e5ff08710290ad7870d6/crates/bevy_wgpu/src/lib.rs#L24
+    //     // update: https://github.com/bevyengine/bevy/blob/0dba0fe45f60cf06e802e5ff08710290ad7870d6/crates/bevy_wgpu/src/wgpu_renderer.rs#L112
+    //     // https://github.com/bevyengine/bevy/blob/master/crates/bevy_wgpu/src/wgpu_renderer.rs#L66
+    //     // winit_window + https://github.com/rust-windowing/glutin/blob/master/glutin_examples/examples/raw_context.rs#L55
+    //     // https://github.com/grovesNL/glow/blob/main/examples/hello/src/main.rs#L73
+    //     // + pub window_surfaces: Arc<RwLock<HashMap<WindowId, wgpu::Surface>>>,
+    // }
 }
 
 #[derive(Default)]
@@ -277,6 +340,12 @@ impl Deref for Font {
     }
 }
 
+impl DerefMut for Font {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
 impl Font {
     pub fn new_from_bytes(buf: &[u8]) -> Font {
         let mut font_loader = doryen::FontLoader::new();
@@ -295,7 +364,8 @@ impl AssetLoader for FontLoader {
         load_context: &'a mut LoadContext,
     ) -> BoxedFuture<'a, Result<(), anyhow::Error>> {
         Box::pin(async move {
-            let font = Font::new_from_bytes(bytes);
+            let mut font = Font::new_from_bytes(bytes);
+            font.set_size_from_name(load_context.path().to_str().unwrap_or_default());
             load_context.set_default_asset(LoadedAsset::new(font));
             Ok(())
         })
